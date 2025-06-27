33bba6dea5253cfac1adc420e80a0694ed43cc83
therenze <therenze.sea@gmail.com>
2025-06-27 13:32:11 +0800

    thesis files upload

--- OnlinePayment/config.php
+++ OnlinePayment/config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "sk_test_4FVIic9QMnsKUSJK6V5KOU1l",
-  "publishable_key" => "pk_test_PClPmBF9bl8qmF4ODNl6Hecp"
+  // "secret_key"      => "sk_test_4FVIic9QMnsKUSJK6V5KOU1l",
+  // "publishable_key" => "pk_test_PClPmBF9bl8qmF4ODNl6Hecp"
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
